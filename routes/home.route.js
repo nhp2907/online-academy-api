c462d089bd954d79ec04e7e26a0374535778ad1e
nhp2907 <40029330+nhp2907@users.noreply.github.com>
2021-07-03 20:37:35 +0700

    add home route

---
+++ routes/home.route.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+const UserService = require("../services/user.service");
+const router = express.Router();
+
+const multer = require('multer')
+const UserModel = require("../schemas/user.schema");
+const UserRole = require("../constant/UserRole");
+
+router.get
+
+module.exports = router
