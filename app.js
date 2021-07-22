3eb6f6fdfc442939b71e36995c0e331dc244cc14
nhp2907 <40029330+nhp2907@users.noreply.github.com>
2021-07-22 18:39:20 +0700

    add webhook

--- app.js
+++ app.js
@@ -27,11 +27,61 @@ app.use('/api/admin',verifyJwt, verifyAdmin, require('./routes/admin.route'));
 app.use('/api/category', require('./routes/category.route'))
 app.use('/api/course', require('./routes/course.route'))
 app.use('/api/learning', verifyJwt, verifyInvoice, require('./routes/learning.route'))
-// app.use('/error', require('./routes/error'));
-// app.use('/test', require('./routes/ test'));
-// app.use('/cart', require('./routes/cart'));
-// app.use('/cart/payment', require('./routes/payment'));
-// app.use('/my-courses', require('./routes/my-course'));
+
+// Creates the endpoint for our webhook
+app.post('/webhook', (req, res) => {
+
+    let body = req.body;
+    console.log(body);
+
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
+
+            // Gets the message. entry.messaging is an array, but
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+        });
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
+
+});
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "nhp2907"
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
+});
 
 app.use(function (err, req, res, next) {
     console.error(err)
